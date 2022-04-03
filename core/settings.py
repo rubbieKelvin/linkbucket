01d9adfbd38a469a58e33b0f797a46ff33110de2
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2022-04-03 17:54:22 +0100

    setting up somewhere else

--- core/settings.py
+++ core/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-m_&#lr5pf2__$@q6p4y+)z1$h67@o7hi&_l^x@o3vq=65!bxgo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')!=None
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
