7511fff7791b513e7f993c0fbdad80909376531c
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2022-04-03 20:22:28 +0100

    made lil changes

--- bucket/models.py
+++ bucket/models.py
@@ -14,8 +14,8 @@ class Tag(models.Model):
 class Link(models.Model):
 	id = models.BigAutoField(primary_key=True)
 	title = models.CharField(max_length=300)
-	link = models.URLField()
-	image = models.URLField(null=True, blank=True)
+	link = models.URLField(max_length=5000)
+	image = models.URLField(null=True, blank=True, max_length=5000)
 	date_posted = models.DateTimeField(auto_now_add=True)
 	public = models.BooleanField(default=True)
 	author = models.ForeignKey('authr.User', related_name='links', on_delete=models.DO_NOTHING, null=True)
@@ -50,7 +50,7 @@ class Vote(models.Model):
 		UPVOTE = "UPVOTE"
 		DOWNVOTE = "DOWNVOTE"
 
-	id = models.UUIDField(default=uuid.uuid4(), primary_key=True)
+	id = models.UUIDField(default=uuid.uuid4, primary_key=True)
 	type = models.CharField(max_length=8, choices=Type.choices)
 	user = models.ForeignKey('authr.User', on_delete=models.CASCADE, related_name='votes')
 	date_voted = models.DateTimeField(auto_now_add=Type)
